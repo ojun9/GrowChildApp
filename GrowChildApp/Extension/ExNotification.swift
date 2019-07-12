44d0f9da61ba95f73ea42d3506c2ce7a0c0226dc
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-07-12 18:40:53 +0900

    add extension file

---
+++ GrowChildApp/Extension/ExNotification.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  GrowChildApp
+//
+//  Created by jun on 2019/07/12.
+//  Copyright © 2019 jun. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+   static let TapTodoImage = Notification.Name("TapTodoImage")
+}
