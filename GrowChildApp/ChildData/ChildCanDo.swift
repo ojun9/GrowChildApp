52321158976d4cce74ed8027b09ec81f03b9ad83
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-07-12 18:38:47 +0900

    add can do

---
+++ GrowChildApp/ChildData/ChildCanDo.swift
@@ -0,0 +1,14 @@
+//
+//  ChildCanDo.swift
+//  GrowChildApp
+//
+//  Created by jun on 2019/07/12.
+//  Copyright © 2019 jun. All rights reserved.
+//
+
+import Foundation
+
+
+class CanDo {
+   
+}
