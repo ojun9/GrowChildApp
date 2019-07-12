29165005d27e576b84c54f617089d455c4dddb4d
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-07-12 18:39:40 +0900

    add breast feeding manager

---
+++ GrowChildApp/ChildData/BreastFeedingManager.swift
@@ -0,0 +1,13 @@
+//
+//  BreastFeedingManager.swift
+//  GrowChildApp
+//
+//  Created by jun on 2019/07/12.
+//  Copyright © 2019 jun. All rights reserved.
+//
+
+import Foundation
+
+class BreastFeedingManager {
+   
+}
