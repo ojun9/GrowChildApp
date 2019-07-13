d7f6aa889ef15afff29f134e5ecaa0e99e4b0d82
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-07-13 12:09:55 +0900

    set can do view controller

---
+++ GrowChildApp/ChildCanDoViewCon/ChildCandoRegisterViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ChildCandoRegisterViewController.swift
+//  GrowChildApp
+//
+//  Created by jun on 2019/07/13.
+//  Copyright © 2019 jun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+import ChameleonFramework
+import TapticEngine
+
+class ChildCandoRegisterViewController: UIViewController {
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+   }
+   
+}
