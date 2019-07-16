142bc6218ddd2a8df76aabd255beaff8ec46aa6f
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-07-16 18:50:26 +0900

    add collectionview

---
+++ GrowChildApp/ChildCanDoViewCon/See/CustomView/SeeCollectionView.swift
@@ -0,0 +1,15 @@
+//
+//  SeeCollectionView.swift
+//  GrowChildApp
+//
+//  Created by jun on 2019/07/16.
+//  Copyright © 2019 jun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class SeeCollectionView: UICollectionView {
+   
+}
