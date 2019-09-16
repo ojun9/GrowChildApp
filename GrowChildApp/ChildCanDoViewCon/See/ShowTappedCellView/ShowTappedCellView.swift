0c81bcbce1b4bb2204be8b46a7e93671539ffba7
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-09-16 12:54:55 +0900

    必要な要素を加えた

--- GrowChildApp/ChildCanDoViewCon/See/ShowTappedCellView/ShowTappedCellView.swift
+++ GrowChildApp/ChildCanDoViewCon/See/ShowTappedCellView/ShowTappedCellView.swift
@@ -9,19 +9,62 @@
 import Foundation
 import UIKit
 import ChameleonFramework
+import SnapKit
 
 class ShowTapeedCellViewController: UIViewController {
-
+   
+   var ViewH: CGFloat = 0
+   var ViewW: CGFloat = 0
+   
+   var TitleLabel = UILabel()
+   var ChildImageView = UIImageView()
+   var MemoView = UITextView()
+   var UserSellectImageView = UIImageView()
+   var AfterMemoView = UITextView()
+   var DeleteButton = UIButton()
+   
    
    override func viewDidLoad() {
       super.viewDidLoad()
-   
-      self.view.backgroundColor = UIColor.flatWhite()
       
+      InitViewSizeInfo()
+   
+      InitTitleLabel()
+      InitChildImage()
+      InitMemoView()
+      InitUserSellectImage()
+      InitAfterMemoView()
+      InitDeleteButton()
       
    }
    
+   func InitViewSizeInfo() {
+      self.ViewH = self.view.frame.height
+      self.ViewW = self.view.frame.width
+   }
    
-  
+   func InitTitleLabel() {
+      TitleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+   }
+   
+   func InitChildImage() {
+        
+   }
+     
+   func InitMemoView() {
+        
+    }
+     
+   func InitUserSellectImage() {
+        
+   }
+     
+   func InitAfterMemoView() {
+        
+   }
+     
+   func InitDeleteButton() {
+        
+   }
      
 }
